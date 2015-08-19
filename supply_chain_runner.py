ec005bb87413a027fa56425a4155b0159acda487
Sushil Shah <sushil.k.shah@accenture.com>
2015-08-19 09:34:04 +0000

    Complete functionality of supply chain completed

--- supply_chain_runner.py
+++ supply_chain_runner.py
@@ -5,8 +5,9 @@ import os
 import ConfigParser
 from atl_pahomqtt import CarriotsMqttClient
 import logging
-import serial, time, json
+import serial, time, json, datetime
 import atl_utils
+from sfdc_utils import SFDCUtils
 
 def main():
 	
@@ -21,7 +22,7 @@ def main():
 	cp.read(os.path.splitext(__file__)[0] + '.ini')
 	log_file = cp.get('PARAMETERS','log_file')
 
-	logging.basicConfig(filename=log_file,filemode='w', level=logging.DEBUG)
+	logging.basicConfig(filename=log_file,filemode='w', level=logging.INFO)
 
 	logging.info('Starting Supply Chain program')
 	logging.info('Config read from: ' + os.path.splitext(__file__)[0] + '.ini')
@@ -42,23 +43,29 @@ def main():
 	start_time = time.time()
 	baudRate = cp.getint('DEVICE_PARAMS','baudRate')
 	connectPort = cp.get('DEVICE_PARAMS','connectPort')
+	device_id = cp.get('DEVICE_PARAMS','device_id')
 
 	'''Serial Read from RFID Reader and Accelerometer'''
 	ser = serial.Serial(connectPort, baudRate)
 	while 1:
 		serial_line = ser.readline()
 		if atl_utils.is_json(serial_line):
-			logging.debug(serial_line)
 			serial_json = json.loads(serial_line)
 			if "rfid_tag" not in serial_json:
-				logging.info( 'accelerometer reading')
+				logging.debug( 'accelerometer reading: %s' %serial_line)
 				elapsed_time = time.time() - start_time
 				if elapsed_time >= post_interval:
 					client_mqtt_post.post_to_carriots(serial_json)
 					start_time = time.time()
 			else:
-				logging.info( 'RFID readings')
-				logging.info( serial_json)
+				logging.debug( 'RFID readings: %s' %serial_line)
+				rfid_tag_id = serial_json["rfid_tag"].lstrip('$')
+				curr_time = str(datetime.datetime.now().isoformat())
+				x = SFDCUtils()
+				payload = '{"ID__c":"'+rfid_tag_id+'","SensorID__c":"'+device_id+'","Type__c":"beef","Status__c":"Transit","ShippingTime__c":"'+curr_time+'"}'
+				logging.debug("payload : %s" %payload)
+				resp = x.post(payload)
+				logging.info("Post response : %s" %resp)
 
 
 main()
